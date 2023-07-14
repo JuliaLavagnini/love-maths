3e1b9465c2d78e778a7e115c13df2030882abeba
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-07-14 13:07:48 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function runGame(){}
+function checkAnswer(){}
+function calculateCorrectAnswer(){}
+function incrementScore(){}
+function incrementWrongScore(){}
+function displayAdditionQuestion(){}
+function displaySubtractQuestion(){}
+function displayMultiplyQuestion(){}
