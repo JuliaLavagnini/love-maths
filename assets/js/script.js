3ead755b38fcb2510724e468b28bc823ab39fc42
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-07-14 13:39:35 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,38 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked sumbit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * Tha main game 'loop', called when the script is first loaded 
  * and after the user's answrr has been processed
  */
-function runGame(){
+function runGame(gameType){
     //Generate two numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer(){}
 function calculateCorrectAnswer(){}
 function incrementScore(){}
 function incrementWrongScore(){}
-function displayAdditionQuestion(){}
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+}
 function displaySubtractQuestion(){}
 function displayMultiplyQuestion(){}
