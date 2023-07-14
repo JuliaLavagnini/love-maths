e1b7d02558e28133bf615921612c75f335e86c14
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2023-07-14 14:12:21 +0100

    Implement multiplication and subtraction questions on the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,5 +73,13 @@ function displayAdditionQuestion(operand1,operand2){
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
 }
-function displaySubtractQuestion(){}
-function displayMultiplyQuestion(){}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "-";
+}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
